232607b622669bbe56f19a58fe27148add651e4a
Robert A. Uhl <robert.uhl@pachyderm.io>
2021-05-05 15:57:00 -0400

    Add s3gateway-port
    Also add a test for ports declared in services which are not named in
    containers.

--- test/pachyderm_helm_test.go
+++ test/pachyderm_helm_test.go
@@ -5,12 +5,13 @@ package helmtest
 
 import (
 	"errors"
-	"fmt"
 	"testing"
 
+	appsV1 "k8s.io/api/apps/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 
 	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/gruntwork-io/terratest/modules/k8s"
@@ -105,7 +106,6 @@ func TestPachdImageTagDeploymentEnv(t *testing.T) {
 	output := helm.RenderTemplate(t, options, helmChartPath, "deployment", []string{"templates/pachd/deployment.yaml"})
 
 	var deployment appsv1.Deployment
-	fmt.Printf("%+v\n", deployment)
 	helm.UnmarshalK8SYaml(t, output, &deployment)
 
 	deploymentContainers := deployment.Spec.Template.Spec.Containers
@@ -362,6 +362,155 @@ func GetEnvVarByName(envVars []v1.EnvVar, name string) (error, string) {
 		}
 	}
 	return errors.New("Not found"), ""
+}
+
+type service struct {
+	name     string
+	selector map[string]string
+	ports    []string
+}
+
+func (s service) match(c container) bool {
+	for k, v := range s.selector {
+		if c.labels[k] != v {
+			return false
+		}
+	}
+	return true
+}
+
+type container struct {
+	name   string
+	index  int
+	labels map[string]string
+	ports  map[string]bool
+}
+
+func TestServicePorts(t *testing.T) {
+	var (
+		hostPath     = "/this/is/a/host/path/"
+		objects, err = manifestToObjects(helm.RenderTemplate(t,
+			&helm.Options{
+				SetStrValues: map[string]string{
+					"pachd.storage.backend":        "LOCAL",
+					"pachd.storage.local.hostPath": hostPath,
+				}},
+			"../pachyderm/", "release-name", nil))
+		services   []service
+		containers []container
+	)
+	if err != nil {
+		t.Fatalf("could not render templates to objects: %v", err)
+	}
+	for _, object := range objects {
+		switch object := object.(type) {
+		case *v1.Service:
+			var ports []string
+			for _, p := range object.Spec.Ports {
+				if p.TargetPort.Type == intstr.String {
+					ports = append(ports, p.TargetPort.StrVal)
+				}
+			}
+			if len(ports) == 0 {
+				continue
+			}
+			services = append(services, service{
+				name:     object.Name,
+				selector: object.Spec.Selector,
+				ports:    ports,
+			})
+		case *appsV1.Deployment:
+			for i, c := range object.Spec.Template.Spec.Containers {
+				var ports = make(map[string]bool)
+				for _, p := range c.Ports {
+					if p.Name != "" {
+						ports[p.Name] = true
+					}
+				}
+				if len(ports) == 0 {
+					continue
+				}
+				containers = append(containers, container{
+					name:   object.Name,
+					index:  i,
+					labels: object.Spec.Template.Labels,
+					ports:  ports,
+				})
+			}
+		case *appsV1.StatefulSet:
+			for i, c := range object.Spec.Template.Spec.Containers {
+				var ports = make(map[string]bool)
+				for _, p := range c.Ports {
+					if p.Name != "" {
+						ports[p.Name] = true
+					}
+				}
+				if len(ports) == 0 {
+					continue
+				}
+				containers = append(containers, container{
+					name:   object.Name,
+					index:  i,
+					labels: object.Spec.Template.Labels,
+					ports:  ports,
+				})
+			}
+		case *v1.ReplicationController:
+			for i, c := range object.Spec.Template.Spec.Containers {
+				var ports = make(map[string]bool)
+				for _, p := range c.Ports {
+					if p.Name != "" {
+						ports[p.Name] = true
+					}
+				}
+				if len(ports) == 0 {
+					continue
+				}
+				containers = append(containers, container{
+					name:   object.Name,
+					index:  i,
+					labels: object.Spec.Template.Labels,
+					ports:  ports,
+				})
+			}
+		case *appsV1.DaemonSet:
+			for i, c := range object.Spec.Template.Spec.Containers {
+				var ports = make(map[string]bool)
+				for _, p := range c.Ports {
+					if p.Name != "" {
+						ports[p.Name] = true
+					}
+				}
+				if len(ports) == 0 {
+					continue
+				}
+				containers = append(containers, container{
+					name:   object.Name,
+					index:  i,
+					labels: object.Spec.Template.Labels,
+					ports:  ports,
+				})
+			}
+		}
+	}
+	for _, s := range services {
+		var satisfied = make(map[string]bool)
+		for _, c := range containers {
+			if !s.match(c) {
+				continue
+			}
+			for _, p := range s.ports {
+				if c.ports[p] {
+					satisfied[p] = true
+				}
+			}
+		}
+		for _, p := range s.ports {
+			if !satisfied[p] {
+				t.Errorf("nothing satisfies service %s/%s", s.name, p)
+			}
+		}
+	}
 }
 
 //Tests todo
